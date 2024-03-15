1c4e19d80341c9b924a19f6ba1c1683f8ccd2799
Nathan Lambertson <lambertones@gmail.com>
2024-03-15 11:43:58 -0500

    Update logic

--- lib/run_mono_synth.rb
+++ lib/run_mono_synth.rb
@@ -7,17 +7,18 @@ mono_synth.play_random_notes(count: 13)
 
 # Play a predefined sequence of notes
 notes = [73, 63, 44, 80, 66, 77]
-velocities = [30, 45, 53, 60, 79, 55]
+velocities = [97, 45, 53, 60, 79, 55]
 
 23.times do
   mono_synth.play_sequence(notes, velocities)
 end
 
 notes = notes.reverse.append(74, 64, 45, 81, 67, 78)
-velocities = (50..111).to_a.take(12)
+velocities = (50..111).to_a.shuffle.take(12)
 
 13.times do
   mono_synth.play_sequence(notes, velocities, duration: 0.11)
 end
 
 mono_synth.play_sequence([75, 63, 44, 37], [55, 47, 67, 99], duration: 1.137)
+mono_synth.play_sequence([30], [111], duration: 5.0)
