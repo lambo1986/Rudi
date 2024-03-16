63ec11f4acd00132f6cc7d5d6cae9e3bd9326521
Nathan Lambertson <lambertones@gmail.com>
2024-03-16 10:48:57 -0500

    Add to Poly runner

--- lib/run_poly_synth.rb
+++ lib/run_poly_synth.rb
@@ -4,10 +4,22 @@ poly_synth = PolySynth.new
 
 # Example: Play a C Major7 chord
 c_major_chord = [60, 64, 67, 71]  # MIDI note numbers for C, E, G, B
+a_minor_chord = [57, 67, 69, 72]
+f_major_chord = [53, 57, 62, 76]
 velocity = 100  # Velocity (loudness) of the chord
 duration = 0.3    # Duration of the chord in seconds
 pause = 0.1     # Pause after playing the chord
 
 5.times do
-  poly_synth.play_chord(c_major_chord, velocity, duration, pause)
+  5.times do
+    poly_synth.play_chord(c_major_chord, velocity, duration, pause)
+  end
+
+  7.times do
+    poly_synth.play_chord(a_minor_chord, velocity, duration, pause)
+  end
+
+  6.times do
+    poly_synth.play_chord(f_major_chord, velocity, duration, pause)
+  end
 end
