77798871384c5c41c99bc53ed1e7180bfa14f8d5
Nathan Lambertson <lambertones@gmail.com>
2024-03-16 10:50:42 -0500

    √

--- lib/run_poly_synth.rb
+++ lib/run_poly_synth.rb
@@ -2,13 +2,12 @@ require_relative 'poly_synth.rb'
 
 poly_synth = PolySynth.new
 
-# Example: Play a C Major7 chord
 c_major_chord = [60, 64, 67, 71]  # MIDI note numbers for C, E, G, B
 a_minor_chord = [57, 67, 69, 72]
 f_major_chord = [53, 57, 62, 76]
-velocity = 100  # Velocity (loudness) of the chord
-duration = 0.3    # Duration of the chord in seconds
-pause = 0.1     # Pause after playing the chord
+velocity = 100
+duration = 0.3
+pause = 0.1
 
 5.times do
   5.times do
